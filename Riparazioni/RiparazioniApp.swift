3863fbf667abc67926f0a1908a06bec994769278
Francesco Romano <francesco.romano.1987@gmail.com>
2023-12-10 14:57:28 +0000

    Empty project for new Riparazioni revision

---
+++ Riparazioni/RiparazioniApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiparazioniApp.swift
+//  Riparazioni
+//
+//  Created by Francesco Romano on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RiparazioniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
