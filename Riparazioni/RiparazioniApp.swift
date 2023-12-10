fa944fc06a1a91d1747b6a76343f26de5cda5700
Francesco Romano <francesco.romano.1987@gmail.com>
2023-12-10 15:11:49 +0000

    Update project to use Firebase Firestore

--- Riparazioni/RiparazioniApp.swift
+++ Riparazioni/RiparazioniApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Swift
+import FirebaseCore
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private func applicationDidFinishLaunching(_ application: NSApplication) -> Void {
+    FirebaseApp.configure()
+  }
+}
 
 @main
 struct RiparazioniApp: App {
+    // Register app delegate for Firebase setup.
+    @NSApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
